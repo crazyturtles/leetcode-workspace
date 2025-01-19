3c6b871366331691c1b239904d8da81986b7f787
Rohan Matharu <rohanmatharu@gmail.com>
2025-01-19 14:55:53 -0700

    Update new-problem.bat

--- lib/new-problem.bat
+++ lib/new-problem.bat
@@ -1,19 +1,19 @@
 @echo off
 if "%1"=="" (
-    echo Usage: new-problem.bat problem-name
-    exit /b 1
+   echo Usage: new-problem.bat problem-name
+   exit /b 1
 )
 
 set PROBLEM_DIR=problems\%1
 mkdir "%PROBLEM_DIR%\test-cases"
 copy lib\template.js "%PROBLEM_DIR%\solution.js"
 copy lib\test.bat "%PROBLEM_DIR%\test.bat"
+type nul > "%PROBLEM_DIR%\test-cases\input1.txt"
+type nul > "%PROBLEM_DIR%\test-cases\output1.txt"
 
 echo # Intuition > "%PROBLEM_DIR%\README.md"
 echo. >> "%PROBLEM_DIR%\README.md"
-echo. >> "%PROBLEM_DIR%\README.md"
 echo # Approach >> "%PROBLEM_DIR%\README.md"
-echo. >> "%PROBLEM_DIR%\README.md"
 echo. >> "%PROBLEM_DIR%\README.md"
 echo # Complexity >> "%PROBLEM_DIR%\README.md"
 echo - Time complexity: O(n) - >> "%PROBLEM_DIR%\README.md"
