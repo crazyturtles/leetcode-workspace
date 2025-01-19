2adc3155c198ed20b89371dd5768af36e71d2c77
Rohan Matharu <rohanmatharu@gmail.com>
2025-01-19 14:52:35 -0700

    Update new-problem.bat

--- lib/new-problem.bat
+++ lib/new-problem.bat
@@ -1,8 +1,7 @@
-@"
 @echo off
 if "%1"=="" (
-   echo Usage: new-problem.bat problem-name
-   exit /b 1
+    echo Usage: new-problem.bat problem-name
+    exit /b 1
 )
 
 set PROBLEM_DIR=problems\%1
@@ -10,24 +9,21 @@ mkdir "%PROBLEM_DIR%\test-cases"
 copy lib\template.js "%PROBLEM_DIR%\solution.js"
 copy lib\test.bat "%PROBLEM_DIR%\test.bat"
 
-(
-echo # Intuition
-echo.
-echo.
-echo # Approach
-echo.
-echo.
-echo # Complexity
-echo - Time complexity: $$O(n)$$ - 
-echo - Space complexity: $$O(1)$$ - 
-echo.
-echo # Code
-echo ```
-echo ```
-) > "%PROBLEM_DIR%\README.md"
+echo # Intuition > "%PROBLEM_DIR%\README.md"
+echo. >> "%PROBLEM_DIR%\README.md"
+echo. >> "%PROBLEM_DIR%\README.md"
+echo # Approach >> "%PROBLEM_DIR%\README.md"
+echo. >> "%PROBLEM_DIR%\README.md"
+echo. >> "%PROBLEM_DIR%\README.md"
+echo # Complexity >> "%PROBLEM_DIR%\README.md"
+echo - Time complexity: O(n) - >> "%PROBLEM_DIR%\README.md"
+echo - Space complexity: O(1) - >> "%PROBLEM_DIR%\README.md"
+echo. >> "%PROBLEM_DIR%\README.md"
+echo # Code >> "%PROBLEM_DIR%\README.md"
+echo ``` >> "%PROBLEM_DIR%\README.md"
+echo ``` >> "%PROBLEM_DIR%\README.md"
 
 echo Created new problem structure in %PROBLEM_DIR%
 echo Add test cases in %PROBLEM_DIR%\test-cases\
 echo Edit solution in %PROBLEM_DIR%\solution.js
 echo Complete documentation in %PROBLEM_DIR%\README.md
-"@ | Set-Content -Path "lib\new-problem.bat"
